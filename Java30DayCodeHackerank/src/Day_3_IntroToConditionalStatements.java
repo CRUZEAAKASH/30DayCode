e1e94d0512c648d28797b1f65aacd20c15e1074f
CRUZEAAKASH <aakash.shinghal@gmail.com>
2018-12-07 10:47:11 +0000

    adding commit message

---
+++ Java30DayCodeHackerank/src/Day_3_IntroToConditionalStatements.java
@@ -0,0 +1,17 @@
+
+public class Day_3_IntroToConditionalStatements {
+private static final Scanner scanner = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        
+        if((N%2!=0) || (N>=6 && N<=20))
+            System.out.print("Weird");
+        else
+            System.out.print("Not Weird");
+        
+        scanner.close();
+    }
+}
