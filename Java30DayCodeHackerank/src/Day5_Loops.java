9f2a910e0a775fdce2dee75f121344b5f6608570
CRUZEAAKASH <aakash.shinghal@gmail.com>
2018-12-11 07:24:53 +0000

    change in class 5 name

--- Java30DayCodeHackerank/src/Day5_Loops.java
+++ Java30DayCodeHackerank/src/Day5_Loops.java
@@ -1,4 +1,13 @@
+import java.util.Scanner;
 
-public class D1y5_Loops {
-
-}
+public class Day5_Loops {
+	private static final Scanner s = new Scanner(System.in);
+	public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int n = s.nextInt();
+        s.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        for(int i=1;i<=10;i++) {
+            System.out.println(n + " x " + i + " = " + i*n);
+        }
+    }
+}	
