f96267d01e6c6dc0e6f8e779d4943eee02903587
CRUZEAAKASH <aakash.shinghal@gmail.com>
2018-12-11 07:23:14 +0000

    added class 4

--- Java30DayCodeHackerank/src/Day3_IntroToConditionalStatements.java
+++ Java30DayCodeHackerank/src/Day3_IntroToConditionalStatements.java
@@ -1,5 +1,6 @@
+import java.util.Scanner;
 
-public class Day_3_IntroToConditionalStatements {
+public class Day3_IntroToConditionalStatements {
 private static final Scanner scanner = new Scanner(System.in);
     
     public static void main(String[] args) {
